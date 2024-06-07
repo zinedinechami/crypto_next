7c8218d6c51b5a5eaf185618e590c41d313865e0
Zinedine Chami <zinedine.chami@gmail.com>
2024-06-07 18:34:45 +0200

    added api url's in a condig folder

---
+++ src/app/config/api.tsx
@@ -0,0 +1,6 @@
+export const CoinList = "https://pro-api.coingecko.com/api/v3/coins/markets";
+
+export const SingleCoin = "https://pro-api.coingecko.com/api/v3/coins/id";
+
+export const TrendingCoin =
+  "https://pro-api.coingecko.com/api/v3/search/trending";
