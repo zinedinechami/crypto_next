d9c6415f391229fe10ff89ccc0d59c4b6b1fa628
Zinedine Chami <zinedine.chami@gmail.com>
2024-06-11 14:57:55 +0200

    fixed all errors in the console, new async client side api fetch function for trending

--- src/app/componenets/trending.tsx
+++ src/app/componenets/trending.tsx
@@ -1,60 +1,59 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { TrendingCoin } from "../config/api";
 
 export default function Trending() {
-  const [TrendingData, setTrendingData] = useState("");
+  const [TrendingData, setTrendingData] = useState();
 
-  const api_url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h";
+  const api_url = "https://api.coingecko.com/api/v3/search/trending";
+
+  const getTrendingData = async () => {
+    try {
+      const response = await fetch(api_url, {
+        method: "GET",
+        headers: { accept: "application/json" },
+      });
+      if (response) {
+        const data = await response.json();
+        if (data) setTrendingData(data);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    fetch(api_url, {
-      method: "GET",
-      headers: { accept: "application/json" },
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setTrendingData(data.body);
-      });
-  });
+    getTrendingData();
+  }, []);
 
-  // i need to do splice with the trending data, ex only show coins from 4 to 10, than passe through data in componenet
-
-  // do a map with the article, find a way to only show the 6 first in the trending list
+  // ? create a splice variable?
 
   return (
     <>
-      <section className="pt-4 pb-6 pl-3 ">
-        <h1 className="text-base mb-4 text-gray-300 ">Trending Coins</h1>
-        <div className="flex flex-wrap justify-between w-full">
-          <article className="w-72 h-24 mb-4 flex rounded-md border border-gray-900 bg-gray-950 hover:bg-gray-900/50 transition-colors">
-            <div className="m-4 flex">
-              <div className="w-16 h-16 rounded-full bg-white "></div>
-              <div className="ml-6">
-                <p className="font-bold">Bitcoin</p>
-                <p className="text-sm p-1 bg-gray-800 text-center rounded-lg my-1">
-                  BTC
-                </p>
+      {TrendingData && (
+        <section className="pt-4 pb-6 pl-3 ">
+          <h1 className="text-base mb-4 text-gray-300 ">Trending Coins</h1>
+          <div className="flex flex-wrap justify-between w-full">
+            <article className="w-72 h-24 mb-4 flex rounded-md border border-gray-900 bg-gray-950 hover:bg-gray-900/50 transition-colors">
+              <div className="m-4 flex">
+                <div className="w-16 h-16 rounded-full bg-white "></div>
+                <div className="ml-6">
+                  <p className="font-bold">Bitcoin</p>
+                  <p className="text-sm p-1 bg-gray-800 text-center rounded-lg my-1">
+                    BTC
+                  </p>
+                </div>
+                <div className="mt-2 ml-4">
+                  {/* price */}
+                  <p className="">$69544</p>
+                  <p className="text-xs text-gray-400">-5.81936 %</p>
+                </div>
               </div>
-              <div className="mt-2 ml-4">
-                {/* price */}
-                <p className="">$69544</p>
-                <p className="text-xs text-gray-400">-5.81936 %</p>
-              </div>
-            </div>
-          </article>
-          <article className="w-72 h-24 rounded-md border border-gray-900 bg-gray-950"></article>
-          <article className="w-72 h-24 mr-10 rounded-md border border-gray-900 bg-gray-950"></article>
-          <article className="w-72 h-24 mb-4  rounded-md border border-gray-900 bg-gray-950"></article>
-          <article className="w-72 h-24 rounded-md border border-gray-900 bg-gray-950"></article>
-          <article className="w-72 h-24 mr-10 rounded-md border border-gray-900 bg-gray-950"></article>
-        </div>
-      </section>
+            </article>
+          </div>
+        </section>
+      )}
     </>
   );
 }
