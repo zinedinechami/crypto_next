71dab4597e7acf88e47104215aa6137341b7c02b
Zinedine Chami <zinedine.chami@gmail.com>
2024-06-30 23:47:19 +0200

    started building out coin page

--- src/app/coins/[coinsId]/page.tsx
+++ src/app/coins/[coinsId]/page.tsx
@@ -1,8 +1,11 @@
+import Header from "@/app/componenets/header";
+
 export default function Page() {
   return (
     <>
-      <body>
-        <div>Coin</div>
+      <body className="bg-gradient-to-br from-gray-950  to-gray-800 text-zinc-100 z-0">
+        <Header />
+        <section className="w-11/12  mx-auto bg-gray-950  border-gray-900 h-96 mt-10 mb-20 rounded-lg"></section>
       </body>
     </>
   );
