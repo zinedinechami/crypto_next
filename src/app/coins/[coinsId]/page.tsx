64f4416de00eee0d1c55a699f3909d9e688823e7
Zinedine Chami <zinedine.chami@gmail.com>
2024-07-04 23:30:35 +0200

    updated design of coin page

--- src/app/coins/[coinsId]/page.tsx
+++ src/app/coins/[coinsId]/page.tsx
@@ -5,7 +5,15 @@ export default function Page() {
     <>
       <body className="bg-zinc-950  text-zinc-100 z-0">
         <Header />
-        <section className="w-11/12  mx-auto bg-zinc-950  border-zinc-800 border h-96 mt-10 mb-20 rounded-lg"></section>
+        <section className="flex w-11/12  mx-auto mt-10">
+          <div className="w-1/3 p-6 border-zinc-800 border h-96 rounded-lg">
+            <h1>#1 Bitcoin</h1>
+
+            <h1 className="text-3xl font-bold">$219992992</h1>
+          </div>
+
+          <div className="w-1/2  mx-auto    h-96  mb-20 "></div>
+        </section>
       </body>
     </>
   );
