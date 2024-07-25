1657556451759279a789d366319c9b3d65a49cf9
Zinedine Chami <zinedine.chami@gmail.com>
2024-07-25 23:39:24 +0200

    retrieved and displayed data from api to coin page

--- src/app/coins/[coinsId]/page.tsx
+++ src/app/coins/[coinsId]/page.tsx
@@ -39,23 +39,26 @@ export default function Page({ params }: { params: { coinsId: string } }) {
   //add market cap, volume 24h, total suplly, max supply
   return (
     <>
-      <body className="  text-zinc-100 bg-gradient-to-br from-zinc-950 to-zinc-900 z-0">
-        <Header />
-        {coinsData && (
+      {coinsData && (
+        <body className="  text-zinc-100 bg-gradient-to-br from-zinc-950 to-zinc-900 z-0">
+          <Header />
+
           <section className="flex w-11/12  mx-auto mt-10 ">
             <div className="w-1/3 p-6 border-zinc-800 border h-96 rounded-lg bg-zinc-950">
-              <h1>#1 Bitcoin</h1>
+              <h1>
+                # {coinsData?.market_cap_rank} {coinsData?.name}
+              </h1>
 
               <h1 className="text-3xl font-bold">$219992992</h1>
-              <p>Market Cap: $23456788</p>
-              <p>Volume 24H: $23456788</p>
-              <p>Total Supply: $23456788</p>
+              <p>Market Cap: {coinsData?.market_data.max_supply}</p>
+              <p>Volume 24H: {coinsData?.market_data.max_supply}</p>
+              <p>Total Supply: {coinsData?.market_data.max_supply}</p>
             </div>
 
             <div className="w-1/2  mx-auto    h-96  mb-20 "></div>
           </section>
-        )}
-      </body>
+        </body>
+      )}
     </>
   );
 }
